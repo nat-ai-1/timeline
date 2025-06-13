5df36e83b29390f9a9ca675ca4541e660cf16850
Nut-Develope <natthawat4404@gmail.com>
2025-06-13 23:35:32 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-function createSnowflake() {
-    const snowflake = document.createElement("div");
-    snowflake.innerHTML = "&#10052;"; 
-    snowflake.classList.add("snowflake");
-    document.body.appendChild(snowflake);
+// function createSnowflake() {
+//     const snowflake = document.createElement("div");
+//     snowflake.innerHTML = "&#10052;"; 
+//     snowflake.classList.add("snowflake");
+//     document.body.appendChild(snowflake);
     
-    const size = Math.random() * 10 + 10;
-    snowflake.style.fontSize = `${size}px`;
+//     const size = Math.random() * 10 + 10;
+//     snowflake.style.fontSize = `${size}px`;
     
-    snowflake.style.left = `${Math.random() * window.innerWidth}px`;
-    snowflake.style.animationDuration = `${Math.random() * 3 + 2}s`;
+//     snowflake.style.left = `${Math.random() * window.innerWidth}px`;
+//     snowflake.style.animationDuration = `${Math.random() * 3 + 2}s`;
     
-    setTimeout(() => {
-        snowflake.remove();
-    }, 5000);
-}
+//     setTimeout(() => {
+//         snowflake.remove();
+//     }, 5000);
+// }
 
-setInterval(createSnowflake, 100);
+// setInterval(createSnowflake, 100);
 
-console.log("DEV BY Natthawat")
+// console.log("DEV BY Natthawat")
